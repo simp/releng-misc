28e7153d79fcee393c7a897f3fde71d84a904f87
Chris Tessmer <chris.tessmer@onyxpoint.com>
2021-07-16 22:26:07 +0000

    WIP GitHub release asset downloader

---
+++ plans/github_download_release_assets.pp
@@ -0,0 +1,62 @@
+# Prints number of Targets from inventory
+#
+# @param targets
+#    By default: `github_repos` group from inventory
+#
+plan releng::github_download_release_assets(
+  TargetSpec $targets = 'github_repos',
+  Stdlib::AbsolutePath $dest_path = system::env('PWD'),
+  Hash $repo_release_list = {
+  },
+  Sensitive[String[1]] $github_api_token = Sensitive.new(system::env('GITHUB_API_TOKEN')),
+){
+  $github_repos = get_targets($targets)
+
+
+  # FIXME currently just downloads latest release
+  $releases_resultset = run_task_with(
+    'http_request', $github_repos, "Get GitHub releases"
+  ) |$repo_target| {
+    {
+      'base_url' => releng::expand_uri( $repo_target.facts['releases_url'], {} ),
+      'method'   => 'get',
+      'headers' => {
+        'Accept'        => 'application/vnd.github.v3+json',
+        'Authorization' => "token ${github_api_token.unwrap}",
+      },
+      'json_endpoint' => true
+    }
+  }
+
+  $release_assets = Hash( $releases_resultset.ok_set.to_data.map |$result| {
+    $rel_data = $result.get('value.body').filter |$rel| {
+      !$rel['draft'] and !$rel['prerelease']
+    }[0].with |$rel| { # take the first result (most recent) # FIXME this would normally filter on the target tag
+      $rel.filter |$key,$v| {
+        $key in ['assets', 'id','tag_name','assets', 'url', 'html_url']
+      }
+    }
+    [$result['target'], $rel_data]
+  })
+
+  # For each release download each asset (filter on/out el7? el8? src?)
+  $release_download_results = $release_assets.map |$repo_name, $release| {
+    out::message("== $repo_name (Release: ${release['tag_name']}")
+    out::verbose("  -- Release page: ${release['html_url']}")
+    $assets = $release['assets']
+    $asset_dl_results = $assets.map |$asset| {
+      out::message("  -- Asset: ${asset['name']}")
+      log::info("  -- Asset URL: ${asset['browser_download_url']}")
+
+      $dl_result = run_command(
+        "curl -o '${dest_path}/${asset['name']}' -sS -L -H '${asset['content']}' '${asset['browser_download_url']}'",
+        'localhost',
+        "Download ${asset['browser_download_url']}"
+      )
+    }
+  }
+
+
+  debug::break()
+  out::message( "Repos: ${github_repos.size}" )
+}
